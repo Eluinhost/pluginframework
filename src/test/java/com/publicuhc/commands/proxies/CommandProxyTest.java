8e81b1ff41453ab38e84c29badd9c3bf5d4b42d7
HowdenG <graham_howden1@yahoo.co.uk>
2014-04-07 09:24:05 +0100

    test for commandproxy

---
+++ src/test/java/com/publicuhc/commands/proxies/CommandProxyTest.java
@@ -0,0 +1,45 @@
+package com.publicuhc.commands.proxies;
+
+import com.publicuhc.commands.requests.CommandRequest;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.MatchResult;
+
+import static org.powermock.api.mockito.PowerMockito.mock;
+
+@RunWith(PowerMockRunner.class)
+public class CommandProxyTest {
+
+    private CommandProxy proxy;
+    private TestTrigger trigger;
+
+    @Test
+    public void testTrigger() throws NoSuchMethodException, ProxyTriggerException {
+        //set up the proxy
+        proxy.setCommandMethod(trigger.getClass().getMethod("triggerMethod",CommandRequest.class));
+        //make a fake request
+        CommandRequest request = mock(CommandRequest.class);
+
+        //trigger the proxy
+        proxy.trigger(request);
+    }
+
+    @Before
+    public void onStartUp() {
+        proxy = new CommandProxy();
+        trigger = mock(TestTrigger.class);
+        proxy.setInstance(trigger);
+    }
+
+    private static class TestTrigger {
+        public void triggerMethod(CommandRequest request){
+        }
+    }
+}
