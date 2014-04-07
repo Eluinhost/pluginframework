296161d060fdd15fe4b4b6b680184187ca770a95
HowdenG <graham_howden1@yahoo.co.uk>
2014-04-07 15:27:41 +0100

    sendertype tests

--- src/test/java/com/publicuhc/commands/requests/SenderTypeTest.java
+++ src/test/java/com/publicuhc/commands/requests/SenderTypeTest.java
@@ -1,7 +1,35 @@
 package com.publicuhc.commands.requests;
 
+import org.bukkit.command.BlockCommandSender;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.command.RemoteConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.powermock.api.mockito.PowerMockito.mock;
+
+@RunWith(PowerMockRunner.class)
 public class SenderTypeTest {
 
-    //TODO fill out tests
+    @Test
+    public void testGetFromCommandSender() {
+        Player player = mock(Player.class);
+        ConsoleCommandSender console = mock(ConsoleCommandSender.class);
+        BlockCommandSender commandblock = mock(BlockCommandSender.class);
+        RemoteConsoleCommandSender remote = mock(RemoteConsoleCommandSender.class);
+
+        assertThat(SenderType.getFromCommandSender(player), is(SenderType.PLAYER));
+        assertThat(SenderType.getFromCommandSender(console), is(SenderType.CONSOLE));
+        assertThat(SenderType.getFromCommandSender(commandblock), is(SenderType.COMMAND_BLOCK));
+        assertThat(SenderType.getFromCommandSender(remote), is(SenderType.REMOTE_CONSOLE));
+
+        CommandSender other = mock(CommandSender.class);
+        assertThat(SenderType.getFromCommandSender(other), is(SenderType.OTHER));
+    }
 
 }
