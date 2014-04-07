7ba9c3a30866ef4053a13a1c11827de15d755d13
HowdenG <graham_howden1@yahoo.co.uk>
2014-04-07 11:16:52 +0100

    add base translate methods

--- src/main/java/com/publicuhc/translate/Translate.java
+++ src/main/java/com/publicuhc/translate/Translate.java
@@ -1,4 +1,30 @@
 package com.publicuhc.translate;
 
+import java.util.Map;
+
 public interface Translate {
+
+    /**
+     * Translate the string with the key given
+     * @param key the key
+     * @return the translated string
+     */
+    String translate(String key);
+
+    /**
+     * Translate the string and replace the vars with the values given
+     * @param key the key
+     * @param vars the map of keys=>values to replace
+     * @return the translated string
+     */
+    String translate(String key, Map<String, String> vars);
+
+    /**
+     * Utility function for translating with 1 var
+     * @param key the key
+     * @param var the var name
+     * @param value the value for the var
+     * @return the translated string
+     */
+    String translate(String key, String var, String value);
 }
