e67c1de52f1f6ae4b1cd4049ead17fb151718296
HowdenG <graham_howden1@yahoo.co.uk>
2014-04-08 11:17:02 +0100

    tests for the basemethodroute

--- src/test/java/com/publicuhc/commands/routing/BaseMethodRouteTest.java
+++ src/test/java/com/publicuhc/commands/routing/BaseMethodRouteTest.java
@@ -1,7 +1,55 @@
 package com.publicuhc.commands.routing;
 
+import com.publicuhc.commands.requests.SenderType;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.regex.Pattern;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.junit.Assert.assertThat;
+import static org.powermock.api.mockito.PowerMockito.mock;
+
+@RunWith(PowerMockRunner.class)
 public class BaseMethodRouteTest {
 
-    //TODO fill out tests
+    private Pattern pattern;
+    private SenderType[] allowed;
+    private String perm;
+    private String baseCommand;
 
+    private BaseMethodRoute route;
+
+    @Test
+    public void testRoute() {
+        assertThat(route.getRoute(), is(sameInstance(pattern)));
+    }
+
+    @Test
+    public void testAllowedTypes() {
+        assertThat(route.getAllowedTypes(), is(sameInstance(allowed)));
+    }
+
+    @Test
+    public void testPermission() {
+        assertThat(route.getPermission(), is(sameInstance(perm)));
+    }
+
+    @Test
+    public void testBaseCommand() {
+        assertThat(route.getBaseCommand(), is(sameInstance(baseCommand)));
+    }
+
+    @Before
+    public void onStartUp() {
+        pattern = mock(Pattern.class);
+        allowed = new SenderType[]{SenderType.PLAYER, SenderType.CONSOLE};
+        perm = "perms";
+        baseCommand = "testcommand";
+
+        route = new BaseMethodRoute(pattern, allowed, perm, baseCommand);
+    }
 }
