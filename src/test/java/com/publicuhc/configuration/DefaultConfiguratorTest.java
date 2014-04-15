5527f2c414b2f458743339962e7a4aa08cd0ed69
HowdenG <graham_howden1@yahoo.co.uk>
2014-04-15 17:02:30 +0100

    add test yml ready for tests setup for defaultconfigurator test

--- src/test/java/com/publicuhc/configuration/DefaultConfiguratorTest.java
+++ src/test/java/com/publicuhc/configuration/DefaultConfiguratorTest.java
@@ -21,8 +21,52 @@
 
 package com.publicuhc.configuration;
 
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.io.File;
+
+import static org.powermock.api.mockito.PowerMockito.doCallRealMethod;
+import static org.powermock.api.mockito.PowerMockito.mock;
+
+
+@RunWith(PowerMockRunner.class)
 public class DefaultConfiguratorTest {
 
-    //TODO fill out tests
+    private DefaultConfigurator m_configurator;
+    private Plugin m_plugin;
+    private File m_dataFolder;
 
+    @Before
+    public void onSetUp() throws Exception {
+        m_plugin = mock(JavaPlugin.class);
+        m_dataFolder = new File("target/testdatafolder");
+        if(m_dataFolder.exists()){
+            deleteDirectory(m_dataFolder);
+        }
+        m_dataFolder.mkdir();
+        //when(m_plugin.getDataFolder()).thenReturn(m_dataFolder); //TODO stackoverflow error for overriding this final method because equals is final too...
+        doCallRealMethod().when(m_plugin).saveResource("test.yml", false);
+        m_configurator = new DefaultConfigurator(m_plugin);
+
+    }
+
+    public boolean deleteDirectory(File path) {
+        if (path.exists()) {
+            File[] files = path.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isDirectory()) {
+                        deleteDirectory(file);
+                    } else {
+                        file.delete();
+                    }
+                }
+            }
+        }
+        return (path.delete());
+    }
 }
