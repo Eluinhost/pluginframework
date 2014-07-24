49d9763b6e05440cee2905a959cb1e42868b9502
Graham <graham_howden1@yahoo.co.uk>
2014-07-24 22:39:38 +0100

    setup very simple method proxy

---
+++ src/main/java/com/publicuhc/pluginframework/commands/routing/DefaultMethodProxy.java
@@ -0,0 +1,51 @@
+package com.publicuhc.pluginframework.commands.routing;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class DefaultMethodProxy implements MethodProxy {
+
+    private Object instance;
+    private Method method;
+
+    /**
+     * Setup a method proxy that will call the given method on the instance given
+     * @param instance the instance to call on
+     * @param method the method to call
+     */
+    public DefaultMethodProxy(Object instance, Method method)
+    {
+        this.instance = instance;
+        this.method = method;
+    }
+
+    @Override
+    public void setInstance(Object instance)
+    {
+        this.instance = instance;
+    }
+
+    @Override
+    public Object getInstance()
+    {
+        return this.instance;
+    }
+
+    @Override
+    public void setMethod(Method method)
+    {
+        this.method = method;
+    }
+
+    @Override
+    public Method getMethod()
+    {
+        return this.method;
+    }
+
+    @Override
+    public Object invoke(Object... args) throws InvocationTargetException, IllegalAccessException
+    {
+        return method.invoke(instance, args);
+    }
+}
---
+++ src/main/java/com/publicuhc/pluginframework/commands/routing/MethodProxy.java
@@ -0,0 +1,37 @@
+package com.publicuhc.pluginframework.commands.routing;
+
+import com.publicuhc.pluginframework.commands.routes.Route;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public interface MethodProxy {
+
+    /**
+     * @param instance the instance to run against
+     */
+    void setInstance(Object instance);
+
+    /**
+     * @return the instance to run with
+     */
+    Object getInstance();
+
+    /**
+     * @param method the method to run on command run
+     */
+    void setMethod(Method method);
+
+    /**
+     * @return the method to run the command on
+     */
+    public Method getMethod();
+
+    /**
+     * Invoke the method with the given arguments
+     *
+     * @param args the args to call the method with
+     * @return the object returned by the method
+     */
+    public Object invoke(Object... args) throws InvocationTargetException, IllegalAccessException;
+}
