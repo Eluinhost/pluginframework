973317fc608593b2e3b0593dd2adb5d53056cc96
Graham <graham_howden1@yahoo.co.uk>
2014-07-26 12:31:25 +0100

    tests for invoking methods

---
+++ src/test/java/com/publicuhc/pluginframework/routing/DefaultMethodProxyTest.java
@@ -0,0 +1,60 @@
+package com.publicuhc.pluginframework.routing;
+
+import oracle.jrockit.jfr.StringConstantPool;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(PowerMockRunner.class)
+public class DefaultMethodProxyTest {
+
+    private TestObject instance;
+
+    @Before
+    public void onStartup()
+    {
+        instance = new TestObject();
+    }
+
+    @Test
+    public void testInvokeNoArg() throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        Method method = TestObject.class.getMethod("getNoArg");
+        DefaultMethodProxy proxy = new DefaultMethodProxy(instance, method);
+
+        Object returns = proxy.invoke();
+
+        assertThat(returns).isInstanceOf(String.class);
+        assertThat(returns).isSameAs(TestObject.TEST_STRING);
+    }
+
+    @Test
+    public void testInvokeWithArg() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = TestObject.class.getMethod("getArg", String.class);
+        DefaultMethodProxy proxy = new DefaultMethodProxy(instance, method);
+
+        Object returns = proxy.invoke("test");
+
+        assertThat(returns).isInstanceOf(String.class);
+        assertThat(returns).isSameAs("test");
+    }
+
+    private class TestObject {
+        public static final String TEST_STRING = "TEST STRING";
+
+        public String getNoArg()
+        {
+            return TEST_STRING;
+        }
+
+        public String getArg(String arg)
+        {
+            return arg;
+        }
+    }
+}
