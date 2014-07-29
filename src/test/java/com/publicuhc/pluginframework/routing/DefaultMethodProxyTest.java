1d25c2dccf9ad07d9ae5a27e9ede829f3c297f8f
HowdenG <graham_howden1@yahoo.co.uk>
2014-07-29 11:16:17 +0100

    tests with Java 7 invokedynamic

--- src/test/java/com/publicuhc/pluginframework/routing/DefaultMethodProxyTest.java
+++ src/test/java/com/publicuhc/pluginframework/routing/DefaultMethodProxyTest.java
@@ -1,18 +1,19 @@
 package com.publicuhc.pluginframework.routing;
 
-import oracle.jrockit.jfr.StringConstantPool;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.lang.invoke.*;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(PowerMockRunner.class)
-public class DefaultMethodProxyTest {
+public class DefaultMethodProxyTest
+{
 
     private TestObject instance;
 
@@ -23,7 +24,8 @@ public class DefaultMethodProxyTest {
     }
 
     @Test
-    public void testInvokeNoArg() throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+    public void testInvokeNoArg() throws InvocationTargetException, IllegalAccessException, NoSuchMethodException
+    {
         Method method = TestObject.class.getMethod("getNoArg");
         DefaultMethodProxy proxy = new DefaultMethodProxy(instance, method);
 
@@ -34,7 +36,8 @@ public class DefaultMethodProxyTest {
     }
 
     @Test
-    public void testInvokeWithArg() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public void testInvokeWithArg() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException
+    {
         Method method = TestObject.class.getMethod("getArg", String.class);
         DefaultMethodProxy proxy = new DefaultMethodProxy(instance, method);
 
@@ -49,18 +52,19 @@ public class DefaultMethodProxyTest {
     {
         TestObject object = new TestObject();
         long start = System.currentTimeMillis();
-        for(int i = 0; i<1000000; i++) {
+        for(int i = 0; i < 1000000; i++) {
             object.getArg("test");
         }
         System.out.println("Regular object, 1 Million invocations: " + (System.currentTimeMillis() - start) + "ms");
     }
 
     @Test
-    public void testSpeedReflection() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public void testSpeedReflection() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException
+    {
         TestObject object = new TestObject();
 
         long start = System.currentTimeMillis();
-        for(int i = 0; i<1000000; i++) {
+        for(int i = 0; i < 1000000; i++) {
             Method method = TestObject.class.getMethod("getArg", String.class);
             method.invoke(object, "test");
         }
@@ -69,19 +73,49 @@ public class DefaultMethodProxyTest {
     }
 
     @Test
-    public void testSpeedReflectionWithCache() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public void testSpeedReflectionWithCache() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException
+    {
         TestObject object = new TestObject();
         Method method = TestObject.class.getMethod("getArg", String.class);
 
         DefaultMethodProxy proxy = new DefaultMethodProxy(object, method);
         long start = System.currentTimeMillis();
-        for(int i = 0; i<1000000; i++) {
+        for(int i = 0; i < 1000000; i++) {
             proxy.invoke("test");
         }
         System.out.println("Reflection with cache, 1 Million invocations: " + (System.currentTimeMillis() - start) + "ms");
     }
 
-    private class TestObject {
+    @Test
+    public void testSpeedInvokeDynamic() throws Throwable
+    {
+        MethodHandle handle = MethodHandles.lookup().findVirtual(TestObject.class, "getArg", MethodType.methodType(String.class, String.class));
+
+        long start = System.currentTimeMillis();
+        for(int i = 0; i < 1000000; i++) {
+            //need to store result or things go crazy
+            String result = (String) handle.invokeExact(new TestObject(), "test");
+        }
+        System.out.println("invokedynamic, 1 Million invocations: " + (System.currentTimeMillis() - start) + "ms");
+    }
+
+    @Test
+    public void testSpeedInvokeDynamicCallsite() throws Throwable
+    {
+        MethodHandle handle = MethodHandles.lookup().findVirtual(TestObject.class, "getArg", MethodType.methodType(String.class, String.class));
+        handle.bindTo(new TestObject());
+        CallSite site = new ConstantCallSite(handle);
+
+        long start = System.currentTimeMillis();
+        for(int i = 0; i < 1000000; i++) {
+            //need to get the result or things go crazy
+            String result = (String) site.dynamicInvoker().invokeExact(new TestObject(), "test");
+        }
+        System.out.println("invokedynamic with callsite, 1 Million invocations: " + (System.currentTimeMillis() - start) + "ms");
+    }
+
+    private class TestObject
+    {
         public static final String TEST_STRING = "TEST STRING";
 
         public String getNoArg()
