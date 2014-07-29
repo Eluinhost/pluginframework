76f9d762be85bf76331d96c42488d187f7ef45df
HowdenG <graham_howden1@yahoo.co.uk>
2014-07-29 12:15:00 +0100

    didnt mean to move these out

--- src/test/java/com/publicuhc/pluginframework/routing/DefaultMethodProxyTest.java
+++ src/test/java/com/publicuhc/pluginframework/routing/DefaultMethodProxyTest.java
@@ -1,8 +1,53 @@
 package com.publicuhc.pluginframework.routing;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(PowerMockRunner.class)
 public class DefaultMethodProxyTest
-{}
+{
+    @Test
+    public void testInvokeNoArg() throws InvocationTargetException, IllegalAccessException, NoSuchMethodException
+    {
+        Method method = TestObject.class.getMethod("getNoArg");
+        DefaultMethodProxy proxy = new DefaultMethodProxy(new TestObject(), method);
+
+        Object returns = proxy.invoke();
+
+        assertThat(returns).isInstanceOf(String.class);
+        assertThat(returns).isSameAs(TestObject.TEST_STRING);
+    }
+
+    @Test
+    public void testInvokeWithArg() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException
+    {
+        Method method = TestObject.class.getMethod("getArg", String.class);
+        DefaultMethodProxy proxy = new DefaultMethodProxy(new TestObject(), method);
+
+        Object returns = proxy.invoke("test");
+
+        assertThat(returns).isInstanceOf(String.class);
+        assertThat(returns).isSameAs("test");
+    }
+
+    public class TestObject
+    {
+        public static final String TEST_STRING = "TEST STRING";
+
+        public String getNoArg()
+        {
+            return TEST_STRING;
+        }
+
+        public String getArg(String arg)
+        {
+            return arg;
+        }
+    }
+}
