9f8302a58091c1c530d8a60633a34d96e6cf51a6
HowdenG <graham_howden1@yahoo.co.uk>
2014-07-30 13:37:51 +0100

    no need to make our own wrapper

--- src/main/java/com/publicuhc/pluginframework/routing/exception/CommandInvocationException.java
+++ src/main/java/com/publicuhc/pluginframework/routing/exception/CommandInvocationException.java
@@ -2,22 +2,11 @@ package com.publicuhc.pluginframework.routing.exception;
 
 public class CommandInvocationException extends Exception
 {
-    private final Throwable wrappedException;
-
-    /**
-     * Creates a new exception caused during a command invocation
-     * @param exception the cause
-     */
-    public CommandInvocationException(Throwable exception)
-    {
-        wrappedException = exception;
+    public CommandInvocationException(String message, Throwable cause) {
+        super(message, cause);
     }
 
-    /**
-     * @return the original cause of the exception
-     */
-    public Throwable getWrappedException()
-    {
-        return wrappedException;
+    public CommandInvocationException(Throwable cause) {
+        super("Exception thrown when running a command method", cause);
     }
 }
