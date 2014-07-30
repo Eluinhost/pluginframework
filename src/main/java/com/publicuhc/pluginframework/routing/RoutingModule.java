27057161bdd97c1a6d9762d9a9f74bf53fb6f62c
HowdenG <graham_howden1@yahoo.co.uk>
2014-07-30 09:03:15 +0100

    routing module

---
+++ src/main/java/com/publicuhc/pluginframework/routing/RoutingModule.java
@@ -0,0 +1,11 @@
+package com.publicuhc.pluginframework.routing;
+
+import com.google.inject.AbstractModule;
+
+public class RoutingModule extends AbstractModule
+{
+    @Override
+    protected void configure() {
+        bind(Router.class).to(DefaultRouter.class);
+    }
+}
