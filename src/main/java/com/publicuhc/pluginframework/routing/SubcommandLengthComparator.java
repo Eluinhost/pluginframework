1ad3377b68357169d180775781b660bb99b19d62
HowdenG <graham_howden1@yahoo.co.uk>
2014-08-26 12:20:34 +0100

    comparator that compares the length of startswith for command routes, orders highest length first

---
+++ src/main/java/com/publicuhc/pluginframework/routing/SubcommandLengthComparator.java
@@ -0,0 +1,45 @@
+/*
+ * SubcommandLengthComparator.java
+ *
+ * Copyright (c) 2014. Graham Howden <graham_howden1 at yahoo.co.uk>.
+ *
+ * This file is part of PluginFramework.
+ *
+ * PluginFramework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PluginFramework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PluginFramework.  If not, see <http ://www.gnu.org/licenses/>.
+ */
+
+package com.publicuhc.pluginframework.routing;
+
+import java.util.Comparator;
+
+class SubcommandLengthComparator implements Comparator<CommandRoute>
+{
+    @Override
+    public int compare(CommandRoute route1, CommandRoute route2)
+    {
+        int r1 = route1.getStartsWith().length;
+        int r2 = route2.getStartsWith().length;
+
+        if(r1 == r2)
+            return 0;
+
+        if(r1 > r2)
+            return -1;
+
+        if(r2 > r1)
+            return 1;
+
+        throw new IllegalStateException();
+    }
+}
