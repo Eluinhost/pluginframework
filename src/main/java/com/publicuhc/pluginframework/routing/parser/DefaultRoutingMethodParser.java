80981123a2cd4273829ddd2157124779cd776cb7
HowdenG <graham_howden1@yahoo.co.uk>
2014-08-26 16:02:22 +0100

    docs

--- src/main/java/com/publicuhc/pluginframework/routing/parser/DefaultRoutingMethodParser.java
+++ src/main/java/com/publicuhc/pluginframework/routing/parser/DefaultRoutingMethodParser.java
@@ -44,8 +44,12 @@ public class DefaultRoutingMethodParser extends RoutingMethodParser
     private final Field converterFieldArguments;
     private final Field converterFieldNonOptions;
 
+    /**
+     * Used to create CommandRoute objects from a method with an @CommandMethod annotation
+     */
     public DefaultRoutingMethodParser()
     {
+        //setup the reflection to get the ValueConverter from the option specs
         Field argConverterField = null;
         Field optionsConverterField = null;
         try {
@@ -63,12 +67,22 @@ public class DefaultRoutingMethodParser extends RoutingMethodParser
         this.converterFieldNonOptions = optionsConverterField;
     }
 
+    /**
+     * Gets a map of option name->converted class, adds 'arguments' for nonOptions. Ignores options without arguments
+     *
+     * @param parser the parser to use to generate the map
+     * @return map of options to their class
+     */
     protected Map<String, Class> getParameters(OptionParser parser)
     {
         Map<String, Class> parameterTypes = new HashMap<String, Class>();
 
         for(Map.Entry<String, OptionSpec<?>> option : parser.recognizedOptions().entrySet()) {
+
+            //get the name of the option
             OptionSpec optionSpec = option.getValue();
+
+            //if its the non options we add it to 'arguments'
             if(optionSpec instanceof NonOptionArgumentSpec) {
                 NonOptionArgumentSpec nonOptionsSpec = (NonOptionArgumentSpec) optionSpec;
                 try {
@@ -79,8 +93,12 @@ public class DefaultRoutingMethodParser extends RoutingMethodParser
                     e.printStackTrace();
                 }
             }
+
+            //if its an an option with an argument we add it as it's option name
             if(optionSpec instanceof ArgumentAcceptingOptionSpec) {
                 ArgumentAcceptingOptionSpec argspec = (ArgumentAcceptingOptionSpec) optionSpec;
+
+                //only do it for options that accept arguments
                 if(argspec.acceptsArguments()) {
                     try {
                         ValueConverter converter = (ValueConverter) converterFieldArguments.get(argspec);
@@ -147,14 +165,16 @@ public class DefaultRoutingMethodParser extends RoutingMethodParser
             optionParser = new OptionParser();
         }
 
+        //check the @commandmethod parameters are all present and correct
         if(!areCommandMethodParametersCorrect(method))
             throw new CommandParseException("Invalid command method parameters at " + method.getName());
 
+        //add the help formatter and add the default help option
         optionParser.formatHelpWith(new BukkitHelpFormatter());
         OptionSpec helpSpec = optionParser.accepts(annotation.helpOption(), "Shows help").forHelp();
 
+        //setup the proxy and create the route
         MethodProxy proxy = new ReflectionMethodProxy(instance, method);
-
         return new DefaultCommandRoute(annotation.command(), annotation.permission(), annotation.allowedSenders(), proxy, optionParser, helpSpec);
     }
 
